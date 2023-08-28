b8214f225bb270c7c71365dab491a8ca3bb5b9d0
Ramael <ramael.bruno@gmail.com>
2023-08-28 10:00:46 +0200

    feat: modify main screen

--- screens/CheckoutScreen.js
+++ screens/CheckoutScreen.js
@@ -9,7 +9,14 @@ const CheckoutScreen = ({navigation}) => {
 
   return (<View style={style.container}>
 
-{visible&&<InformationComponent setVisible={setVisible}/>}
+{visible&&<InformationComponent     
+    typeOfNotification={"typeOfNotification"}
+    title={"Kouté sa!"}
+    description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."}
+    appearUrl={true}
+    url={"https://google.com"}
+    setVisible={setVisible}
+    />}
 
   <Image 
   resizeMode="contain"
